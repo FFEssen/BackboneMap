c109054c772a85c16a8b15e8af9432faed56c8eb
Nils Martin Klünder <nomoketo@nomoketo.de>
2015-06-25 11:11:38 +0200

    Bochold

---
+++ edit.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+from flask import Flask, request
+from make import make_leaflet
+import webbrowser
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return open('index.html').read()
+
+@app.route('/building', methods=('POST', ))
+def building():
+    print(request.form['title'])
+    stadtteil = request.form['stadtteil'].title()
+    title = request.form['title']
+    coords = request.form['coords']
+    filename = 'data/buildings/stadt/'+stadtteil+'-'+title.replace('-', ' ').title().replace(' ', '')
+    try:
+        open(filename, 'r')
+    except:
+        pass
+    else:
+        return ''
+
+    f = open(filename, 'w')
+    f.write(title+'\n'+coords+'\n\nKeine Notizen.')
+    f.close()
+
+    make_leaflet()
+    return 'ok'
+
+make_leaflet()
+
+app.run(debug=True)
--- make.py
+++ make.py
@@ -6,16 +6,17 @@ buildings_color = {'stadt': '#FF0000'}
 
 
 #L.polygon([[51.460852,6.945248],[51.458285,6.957264],[51.449942,6.953487],[51.450905,6.946106]], { weight: 0, fillColor: '#ff0000', fillOpacity: 0.4 }).addTo(map).bindPopup("I am a polygon.");
+def make_leaflet():
+    add = []
 
-add = []
+    buildingsdir = './data/buildings/'
+    for (dirpath, dirnames, filenames) in os.walk('./data/buildings/'):
+        for filename in filenames:
+            data = open(os.path.join(dirpath, filename)).read()
+            title, latlng, description = data.split('\n', 2)
+            add.append("L.polygon(%s, {weight: 0, fillColor: '%s', fillOpacity: 0.4}).addTo(map).bindPopup('<strong>%s</strong><br><br>%s');" % (latlng, buildings_color[dirpath[len(buildingsdir):]], title, description.strip().replace('\n', '<br>')))
 
-buildingsdir = './data/buildings/'
-for (dirpath, dirnames, filenames) in os.walk('./data/buildings/'):
-    for filename in filenames:
-        data = open(os.path.join(dirpath, filename)).read()
-        title, latlng, description = data.split('\n', 2)
-        add.append("L.polygon(%s, {weight: 0, fillColor: '%s', fillOpacity: 0.4}).addTo(map).bindPopup('<strong>%s</strong><br><br>%s');" % (latlng, buildings_color[dirpath[len(buildingsdir):]], title, description.strip().replace('\n', '<br>')))
+    open('index.html', 'w').write(open('index.tpl.html').read().replace('/* add */', '\n'.join(add)))
 
-open('index.html', 'w').write(open('index.tpl.html').read().replace('/* add */', '\n'.join(add)))
-
-print(add)
+if __name__ == '__main__':
+    make_leaflet()
